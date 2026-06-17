08e43c423699fed4402897a18d2440d04e8c5504
gukamaroski-star <gukamaroski@gmail.com>
2026-06-17 15:03:25 -0300

    Trigger render deploy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Deploy: 2026-06-17 14:48 - Explora Pet Sistema v1.1
+# Deploy: 2026-06-17 15:04 - Explora Pet Sistema v1.1 (Auto Deploy Test)
 from fastapi import FastAPI, Depends, HTTPException, Query, Request, BackgroundTasks
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
